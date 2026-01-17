96f19fc945c0c0b833a23776bb1b5d030666c29d
KING <matadeenyadav202@gmail.com>
2026-01-17 16:03:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// let a = 10;
+// let b = 3;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// let x = 5;
+// x = x+3;
+// console.log(x);
+// x += 3;
+// console.log(x);
+// x -= 3;
+// console.log(x);
+// x *= 3;
+// console.log(x);
+// x /= 3;
+// console.log(x);
+// x %= 3;
+// console.log(x);
+
+// let count = 5;
+// console.log(count)
+// count++
+// console.log(count)
+// ++count
+// console.log(count)
+// count--
+// console.log(count)
+// --count
+// console.log(count)
+
+
+// console.log(5 == "5")
+// console.log(5 === "5")
+
+// let x = 11;
+// if(x>5 && x<20 && x===10){
+//     console.log("x = 10")
+// }
+// else{
+//     console.log("x is not 10.")
+// }
+// tried : true && false , true || false
+// if(!(true)){
+//     console.log("True.")
+// }
+// else{
+//     console.log("False")
+// }
+
+// if(5>3 && 10>8){
+//     console.log("True.")
+// }
+// else{
+//     console.log("False")
+// }
+// if(5>3 || 10<8){
+//     console.log("True.")
+// }
+// else{
+//     console.log("False")
+// }
+
+// Variable Hoisting in JavaScript:-
+
+// console.log(a)
+// var a = 10;
+// let a = 10; : error as  , cannot access a before initialisation.
+
+// test();
+// function test(){
+    // console.log("Hello");
+// }  // functions can be callled before creation and gives output.
+
+
+// try writing a function before initialisation
+hello()
+var hello = function() {
+    console.log("Hi");
+}
